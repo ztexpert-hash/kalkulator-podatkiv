68c5f3ac74138618a2924db933f200e1be8af195
ztexpert-hash <zt.expert@gmail.com>
2026-06-10 21:38:56 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kalkulator-v3';
+const CACHE = 'kalkulator-v4';
 const FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
